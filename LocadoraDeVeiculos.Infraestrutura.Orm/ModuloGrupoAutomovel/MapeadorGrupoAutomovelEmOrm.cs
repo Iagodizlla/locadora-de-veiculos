654bd5d1a20a1359456d044198fe0cf75f37f14b
iagodizlla <iagoschlemper@gmail.com>
2025-11-18 16:23:57 -0300

    Implementado Grupo de Automovel em Infraestrutura

---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloGrupoAutomovel/MapeadorGrupoAutomovelEmOrm.cs
@@ -0,0 +1,27 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloGrupoAutomovel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloGrupoAutomovel;
+
+public class MapeadorGrupoAutomovelEmOrm : IEntityTypeConfiguration<GrupoAutomovel>
+{
+    public void Configure(EntityTypeBuilder<GrupoAutomovel> modelBuilder)
+    {
+        modelBuilder.ToTable("TBGrupoAutomovel");
+
+        modelBuilder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        modelBuilder.Property(b => b.Nome)
+            .HasColumnType("nvarchar(100)")
+            .IsRequired();
+
+        modelBuilder
+            .HasOne(a => a.Usuario)
+            .WithMany()
+            .HasForeignKey(a => a.UsuarioId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloGrupoAutomovel/RepositorioGrupoAutomovelEmOrm.cs
@@ -0,0 +1,8 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoAutomovel;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloGrupoAutomovel;
+
+public class RepositorioGrupoAutomovelEmOrm(IContextoPersistencia context)
+    : RepositorioBase<GrupoAutomovel>(context), IRepositorioGrupoAutomovel;
