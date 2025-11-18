05ba2117c4f1d5298a93e7ba76bbcb4140d36760
iagodizlla <iagoschlemper@gmail.com>
2025-11-18 14:19:27 -0300

    Implementado Funcionario em Infraestrutura

---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloFuncionario/FuncionarioMedicoEmOrm.cs
@@ -0,0 +1,35 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloFuncionario;
+
+public class FuncionarioMedicoEmOrm : IEntityTypeConfiguration<Funcionario>
+{
+    public void Configure(EntityTypeBuilder<Funcionario> modelBuilder)
+    {
+        modelBuilder.ToTable("TBFuncionario");
+
+        modelBuilder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        modelBuilder.Property(b => b.Nome)
+            .HasColumnType("nvarchar(100)")
+            .IsRequired();
+
+        modelBuilder.Property(b => b.Salario)
+            .HasColumnType("float")
+            .IsRequired();
+
+        modelBuilder.Property(b => b.Admissao)
+            .HasColumnType("datetimeoffset")
+            .IsRequired();
+
+        modelBuilder
+            .HasOne(a => a.Usuario)
+            .WithMany()
+            .HasForeignKey(a => a.UsuarioId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloFuncionario/RepositorioFuncionarioEmOrm.cs
@@ -0,0 +1,8 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloFuncionario;
+
+public class RepositorioFuncionarioEmOrm(IContextoPersistencia context) 
+    : RepositorioBase<Funcionario>(context), IRepositorioFuncionario;
