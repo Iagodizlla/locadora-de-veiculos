a4e0bb15ccbb5ffb49e4182c03c44ad1054d4f47
iagodizlla <iagoschlemper@gmail.com>
2025-11-18 16:32:05 -0300

    Implementado Migration de Grupo de Automovel

---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/Migrations/20251118193047_Tabela_Grupo_Automovel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tabela_Grupo_Automovel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBGrupoAutomovel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBGrupoAutomovel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBGrupoAutomovel_AspNetUsers_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBGrupoAutomovel_UsuarioId",
+                table: "TBGrupoAutomovel",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBGrupoAutomovel");
+        }
+    }
+}
