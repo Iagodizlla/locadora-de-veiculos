18d36d8d65e2d8f659fd994d08b640ebfb3e4d2c
iagodizlla <iagoschlemper@gmail.com>
2025-11-19 14:38:17 -0300

    Implementado Endereco de Cliente em Dominio como arquivo separado

---
+++ LocadoraDeVeiculos.Dominio/ModuloCliente/Endereco.cs
@@ -0,0 +1,19 @@
+﻿namespace LocadoraDeVeiculos.Dominio.ModuloCliente;
+
+public class Endereco
+{
+    public string Logradouro { get; set; }
+    public string Numero { get; set; }
+    public string Bairro { get; set; }
+    public string Cidade { get; set; }
+    public string Estado { get; set; }
+    public Endereco() { }
+    public Endereco(string logradouro, string numero, string bairro, string cidade, string estado) : this()
+    {
+        Logradouro = logradouro;
+        Numero = numero;
+        Bairro = bairro;
+        Cidade = cidade;
+        Estado = estado;
+    }
+}
