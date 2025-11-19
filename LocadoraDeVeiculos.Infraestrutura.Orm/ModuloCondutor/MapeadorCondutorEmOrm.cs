83c34b2c8fc3cdef2fcc42b1f45cb59a3d578f0a
iagodizlla <iagoschlemper@gmail.com>
2025-11-19 02:57:16 -0300

    Implementado Condutor em Infraestrutura

---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloCondutor/MapeadorCondutorEmOrm.cs
@@ -0,0 +1,40 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCondutor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloCondutor;
+
+public class MapeadorCondutorEmOrm : IEntityTypeConfiguration<Condutor>
+{
+    public void Configure(EntityTypeBuilder<Condutor> modelBuilder)
+    {
+        modelBuilder.ToTable("TBCondutor");
+
+        modelBuilder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        modelBuilder.Property(c => c.Nome)
+            .IsRequired()
+            .HasColumnType("nvarchar(100)");
+
+        modelBuilder.Property(c => c.Cnh)
+            .IsRequired()
+            .HasColumnType("nvarchar(20)");
+
+        modelBuilder.Property(c => c.Categoria)
+            .IsRequired()
+            .HasColumnType("int");
+
+        modelBuilder.Property(c => c.ValidadeCnh)
+            .IsRequired()
+            .HasColumnType("datetimeoffset");
+
+        modelBuilder
+            .HasOne(a => a.Usuario)
+            .WithMany()
+            .HasForeignKey(a => a.UsuarioId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloCondutor/RepositorioCondutorEmOrm.cs
@@ -0,0 +1,8 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloCondutor;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloCondutor;
+
+public class RepositorioCondutorEmOrm(IContextoPersistencia context)
+    : RepositorioBase<Condutor>(context), IRepositorioCondutor;
