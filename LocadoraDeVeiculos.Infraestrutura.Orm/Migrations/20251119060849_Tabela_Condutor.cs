42e4478fbaf76644e8e615660893b90a27afe310
iagodizlla <iagoschlemper@gmail.com>
2025-11-19 03:12:01 -0300

    Implementado Migration de Condutor

---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/Migrations/20251119060849_Tabela_Condutor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tabela_Condutor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBCondutor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Cnh = table.Column<string>(type: "nvarchar(20)", nullable: false),
+                    Categoria = table.Column<int>(type: "int", nullable: false),
+                    ValidadeCnh = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBCondutor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBCondutor_AspNetUsers_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBCondutor_UsuarioId",
+                table: "TBCondutor",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBCondutor");
+        }
+    }
+}
