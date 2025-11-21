19e400f6fbf84affaae9d22ec36f828ab2407405
iagodizlla <iagoschlemper@gmail.com>
2025-11-21 01:39:53 -0300

    Colocado Plano como Classe filha de EntidadeBase e criado atributo Id de GrupoAutomovel nele

--- LocadoraDeVeiculos.Dominio/ModuloPlano/Plano.cs
+++ LocadoraDeVeiculos.Dominio/ModuloPlano/Plano.cs
@@ -1,8 +1,9 @@
-﻿using LocadoraDeVeiculos.Dominio.ModuloGrupoAutomovel;
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoAutomovel;
 
 namespace LocadoraDeVeiculos.Dominio.ModuloPlano;
 
-public class Plano
+public class Plano : EntidadeBase
 {
     public ETipoPlano TipoPlano { get; set; }
     public GrupoAutomovel GrupoAutomovel { get; set; }
@@ -11,6 +12,7 @@ public class Plano
     public double? KmLivres { get; set; }
     public double? PrecoPorKmExplorado { get; set; }
     public double? PrecoLivre { get; set; }
+    public Guid GrupoAutomovelId { get; set; }
 
     public Plano() { }
     public Plano(ETipoPlano tipoPlano, GrupoAutomovel grupoAutomovel, double? precoDiaria, double? precoPorKm, double? kmLivres, double? precoPorKmExplorado, double? precoLivre) : this()
