b8c48566fe00c579f6d7a756ac27fa13f89332bb
iagodizlla <iagoschlemper@gmail.com>
2025-11-20 22:25:11 -0300

    Implementado Migration de Cliente

--- LocadoraDeVeiculos.Infraestrutura.Orm/Migrations/20251121012440_Tabela_Cliente.Designer.cs
+++ LocadoraDeVeiculos.Infraestrutura.Orm/Migrations/20251121012440_Tabela_Cliente.Designer.cs
@@ -4,6 +4,7 @@ using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LocadoraDeVeiculos.Infraestrutura.Orm.Migrations
 {
     [DbContext(typeof(LocadoraVeiculoDbContext))]
-    partial class LocadoraVeiculoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251121012440_Tabela_Cliente")]
+    partial class Tabela_Cliente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
