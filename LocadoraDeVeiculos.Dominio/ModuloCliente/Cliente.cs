ec41357713fe6c8a0c3f9698bd630de703ad5e3e
iagodizlla <iagoschlemper@gmail.com>
2025-11-20 21:00:25 -0300

    Alterações na classe Cliente para remover as propriedades `Cpf`, `Rg` e `Cnpj`

--- LocadoraDeVeiculos.Dominio/ModuloCliente/Cliente.cs
+++ LocadoraDeVeiculos.Dominio/ModuloCliente/Cliente.cs
@@ -9,23 +9,17 @@ public class Cliente : EntidadeBase
     public string Telefone { get; set; }
     public ETipoCliente ClienteTipo { get; set; }
     public string Documento { get; set; }
-    public string? Cpf { get; set; }
-    public string? Rg { get; set; }
     public string? Cnh { get; set; }
-    public string? Cnpj { get; set; }
 
     public Cliente() { }
 
-    public Cliente(string nome, Endereco endereco, string telefone, ETipoCliente clienteTipo, string documento, string? cnpj, string? rg , string? cnh, string? cpf) : this()
+    public Cliente(string nome, Endereco endereco, string telefone, ETipoCliente clienteTipo, string documento, string? cnh) : this()
     {
         Nome = nome;
         Endereco = endereco;
         Telefone = telefone;
         ClienteTipo = clienteTipo;
         Documento = documento;
-        Cnpj = cnpj;
-        Rg = rg;
         Cnh = cnh;
-        Cpf = cpf;
     }
 }
