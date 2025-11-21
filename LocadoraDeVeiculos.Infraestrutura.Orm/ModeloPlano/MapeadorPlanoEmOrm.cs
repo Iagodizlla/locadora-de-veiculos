baf3b1301fd7d46e60fca049ca530a9f19f6d7cb
iagodizlla <iagoschlemper@gmail.com>
2025-11-21 01:46:35 -0300

    Implementado Plano de Cobranca em Infraestrutura

---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModeloPlano/MapeadorPlanoEmOrm.cs
@@ -0,0 +1,53 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloPlano;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloPlano;
+
+public class MapeadorPlanoEmOrm : IEntityTypeConfiguration<Plano>
+{
+    public void Configure(EntityTypeBuilder<Plano> modelBuilder)
+    {
+        modelBuilder.ToTable("TBPlano");
+
+        modelBuilder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        modelBuilder.Property(b => b.TipoPlano)
+            .HasColumnType("int")
+            .IsRequired();
+
+        modelBuilder.Property(b => b.PrecoDiario)
+            .HasColumnType("float")
+            .IsRequired(false);
+
+        modelBuilder.Property(b => b.PrecoPorKm)
+            .HasColumnType("float")
+            .IsRequired(false);
+
+        modelBuilder.Property(b => b.KmLivres)
+            .HasColumnType("float")
+            .IsRequired(false);
+
+        modelBuilder.Property(b => b.PrecoPorKmExplorado)
+            .HasColumnType("float")
+            .IsRequired(false);
+
+        modelBuilder.Property(b => b.PrecoLivre)
+            .HasColumnType("float")
+            .IsRequired(false);
+
+        modelBuilder.HasOne(a => a.GrupoAutomovel)
+            .WithMany()
+            .HasForeignKey(a => a.GrupoAutomovelId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder
+            .HasOne(a => a.Empresa)
+            .WithMany()
+            .HasForeignKey(a => a.EmpresaId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModeloPlano/RepositorioPlanoEmOrm.cs
@@ -0,0 +1,24 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloPlano;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloPlano;
+
+public class RepositorioPlanoEmOrm(IContextoPersistencia context)
+    : RepositorioBase<Plano>(context), IRepositorioPlano
+{
+    public override async Task<List<Plano>> SelecionarTodosAsync()
+    {
+        return await registros
+            .Include(a => a.GrupoAutomovel)
+            .ToListAsync();
+    }
+
+    public override async Task<Plano?> SelecionarPorIdAsync(Guid id)
+    {
+        return await registros
+            .Include(a => a.GrupoAutomovel)
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
