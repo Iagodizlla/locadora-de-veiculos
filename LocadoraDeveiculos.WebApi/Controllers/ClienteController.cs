07202b28758429179c2f82bdd91a2b547e41e5c4
iagodizlla <iagoschlemper@gmail.com>
2025-11-20 22:06:33 -0300

    Implementado Cliente em Controller

---
+++ LocadoraDeveiculos.WebApi/Controllers/ClienteController.cs
@@ -0,0 +1,77 @@
+﻿using LocadoraDeVeiculos.Aplicacao.ModuloCliente.Commands.Editar;
+using LocadoraDeVeiculos.Aplicacao.ModuloCliente.Commands.Excluir;
+using LocadoraDeVeiculos.Aplicacao.ModuloCliente.Commands.Inserir;
+using LocadoraDeVeiculos.Aplicacao.ModuloCliente.Commands.SelecionarPorId;
+using LocadoraDeVeiculos.Aplicacao.ModuloCliente.Commands.SelecionarTodos;
+using LocadoraDeVeiculos.WebApi.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocadoraDeVeiculos.WebApi.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/clientes")]
+public class ClienteController(IMediator mediator) : ControllerBase
+{
+
+    [HttpPost]
+    [ProducesResponseType(typeof(InserirClienteResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Inserir(InserirClienteRequest request)
+    {
+        var resultado = await mediator.Send(request);
+
+        return resultado.ToHttpResponse();
+    }
+
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(typeof(EditarClienteResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Editar(Guid id, EditarClientePartialRequest request)
+    {
+        var editarRequest = new EditarClienteRequest(
+            id,
+            request.Nome,
+            request.Endereco,
+            request.Telefone,
+            request.TipoCliente,
+            request.Documento,
+            request.Cnh
+        );
+
+        var resultado = await mediator.Send(editarRequest);
+
+        return resultado.ToHttpResponse();
+    }
+
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(typeof(ExcluirClienteResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Excluir(Guid id)
+    {
+        var excluirRequest = new ExcluirClienteRequest(id);
+
+        var resultado = await mediator.Send(excluirRequest);
+
+        return resultado.ToHttpResponse();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(SelecionarClientesResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> SelecionarTodos()
+    {
+        var resultado = await mediator.Send(new SelecionarClientesRequest());
+
+        return resultado.ToHttpResponse();
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(SelecionarClientePorIdResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> SelecionarPorId(Guid id)
+    {
+        var selecionarPorIdRequest = new SelecionarClientePorIdRequest(id);
+
+        var resultado = await mediator.Send(selecionarPorIdRequest);
+
+        return resultado.ToHttpResponse();
+    }
+}
