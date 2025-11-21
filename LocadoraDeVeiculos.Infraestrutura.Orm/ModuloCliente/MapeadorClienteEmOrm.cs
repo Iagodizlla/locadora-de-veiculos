6c6956174780bd8f355e489addbcf95e4d92a9bb
iagodizlla <iagoschlemper@gmail.com>
2025-11-20 22:03:51 -0300

    Implementado Cliente em Infraestrutura

---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloCliente/MapeadorClienteEmOrm.cs
@@ -0,0 +1,47 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloCliente;
+
+public class MapeadorClienteEmOrm : IEntityTypeConfiguration<Cliente>
+{
+    public void Configure(EntityTypeBuilder<Cliente> modelBuilder)
+    {
+        modelBuilder.ToTable("TBCliente");
+
+        modelBuilder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        modelBuilder.Property(b => b.Nome)
+            .HasColumnType("nvarchar(100)")
+            .IsRequired();
+
+        modelBuilder.HasOne(a => a.Endereco)
+            .WithMany()
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Property(b => b.Telefone)
+            .HasColumnType("nvarchar(20)")
+            .IsRequired();
+
+        modelBuilder.Property(b => b.ClienteTipo)
+            .HasColumnType("int")
+            .IsRequired();
+
+        modelBuilder.Property(b => b.Documento)
+            .HasColumnType("nvarchar(20)")
+            .IsRequired();
+
+        modelBuilder.Property(b => b.Cnh)
+            .HasColumnType("nvarchar(20)");
+
+        modelBuilder
+            .HasOne(a => a.Empresa)
+            .WithMany()
+            .HasForeignKey(a => a.EmpresaId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloCliente/RepositorioClienteEmOrm.cs
@@ -0,0 +1,8 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloGrupoCliente;
+
+public class RepositorioClienteEmOrm(IContextoPersistencia context)
+    : RepositorioBase<Cliente>(context), IRepositorioCliente;
