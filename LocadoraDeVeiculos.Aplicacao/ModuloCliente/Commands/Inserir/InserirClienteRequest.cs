4c11260bef4dfc5d9f1cc89142167f22e8a005b5
iagodizlla <iagoschlemper@gmail.com>
2025-11-20 23:40:17 -0300

    Implementado InserirEnderecoRequest

--- LocadoraDeVeiculos.Aplicacao/ModuloCliente/Commands/Inserir/InserirClienteRequest.cs
+++ LocadoraDeVeiculos.Aplicacao/ModuloCliente/Commands/Inserir/InserirClienteRequest.cs
@@ -6,3 +6,5 @@ namespace LocadoraDeVeiculos.Aplicacao.ModuloCliente.Commands.Inserir;
 
 public record InserirClienteRequest(string Nome, Endereco Endereco, string Telefone, ETipoCliente TipoCliente, string Documento, string? Cnh)
     : IRequest<Result<InserirClienteResponse>>;
+
+public record InserirEnderecoRequest(string Logradouro, int Numero, string Bairro, string Cidade, string Estado);
