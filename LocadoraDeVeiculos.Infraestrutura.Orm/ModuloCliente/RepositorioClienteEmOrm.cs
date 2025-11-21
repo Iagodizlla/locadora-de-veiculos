fab84ef49b8b41d61af99e434bfa1f15ba45a64f
iagodizlla <iagoschlemper@gmail.com>
2025-11-20 22:21:12 -0300

    Implementado Endereco como classe filha de EntidadeBase

--- LocadoraDeVeiculos.Infraestrutura.Orm/ModuloCliente/RepositorioClienteEmOrm.cs
+++ LocadoraDeVeiculos.Infraestrutura.Orm/ModuloCliente/RepositorioClienteEmOrm.cs
@@ -1,8 +1,24 @@
 ﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
 using LocadoraDeVeiculos.Dominio.ModuloCliente;
 using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
 
 namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloGrupoCliente;
 
 public class RepositorioClienteEmOrm(IContextoPersistencia context)
-    : RepositorioBase<Cliente>(context), IRepositorioCliente;
+    : RepositorioBase<Cliente>(context), IRepositorioCliente
+{
+    public override async Task<List<Cliente>> SelecionarTodosAsync()
+    {
+        return await registros
+            .Include(a => a.Endereco)
+            .ToListAsync();
+    }
+
+    public override async Task<Cliente?> SelecionarPorIdAsync(Guid id)
+    {
+        return await registros
+            .Include(a => a.Endereco)
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
