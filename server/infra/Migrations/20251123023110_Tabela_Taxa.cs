1c1273a49ee152ed85ea5ddde4c85d835898c8a2
iagodizlla <iagoschlemper@gmail.com>
2025-11-22 23:31:43 -0300

    Implementado Migration de Taxa

---
+++ server/infra/Migrations/20251123023110_Tabela_Taxa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tabela_Taxa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBTaxa",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Servico = table.Column<int>(type: "int", nullable: false),
+                    EmpresaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBTaxa", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBTaxa_AspNetUsers_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBTaxa_EmpresaId",
+                table: "TBTaxa",
+                column: "EmpresaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBTaxa");
+        }
+    }
+}
