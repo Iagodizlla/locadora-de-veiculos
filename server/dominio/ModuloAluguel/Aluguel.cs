c3450836a2eeb745558ffbf160c9b7ecd7800a75
iagodizlla <iagoschlemper@gmail.com>
2025-11-23 17:18:48 -0300

    Arrumado Dominio de Aluguel

--- server/dominio/ModuloAluguel/Aluguel.cs
+++ server/dominio/ModuloAluguel/Aluguel.cs
@@ -8,27 +8,38 @@ namespace LocadoraDeVeiculos.Dominio.ModuloAluguel;
 
 public class Aluguel : EntidadeBase
 {
+    #region 
+    #region Entidades
     public Condutor Condutor { get; set; }
     public Automovel Automovel { get; set; }
     public Plano Plano { get; set; }
     public List<Taxa> Taxas { get; set; }
+    public Guid AutomovelId { get; set; }
+    public Guid CondutorId { get; set; }
+    public Guid PlanoId { get; set; }
+    #endregion
+    #region Datas
     public DateTimeOffset DataSaida { get; set; }
     public DateTimeOffset DataRetornoPrevista { get; set; }
     public DateTimeOffset? DataDevolucao { get; set; }
+    #endregion
+    #region Km e Combustivel
     public int QuilometragemInicial { get; set; }
     public int? QuilometragemFinal { get; set; }
     public int NivelCombustivelNaSaida { get; set; }
     public int? NivelCombustivelNaDevolucao { get; set; }
-    public bool? SeguroCliente { get; set; }
-    public bool? SeguroTerceiro { get; set; }
+    #endregion
+    #region Seguros
+    public bool SeguroCliente { get; set; }
+    public bool SeguroTerceiro { get; set; }
     public double? ValorSeguroPorDia { get; set; }
+    #endregion
     public bool Status { get; set; }
-    public Guid AutomovelId { get; set; }
-    public Guid CondutorId { get; set; }
-    public Guid PlanoId { get; set; }
-
+    #endregion
     public Aluguel() { }
-    public Aluguel(Condutor condutor, Automovel automovel, Plano plano, List<Taxa> taxas, DateTimeOffset dataSaida, DateTimeOffset dataRetornoPrevista, int quilometragemInicial, int nivelCombustivelNaSaida, bool status) : this()
+    public Aluguel(Condutor condutor, Automovel automovel, Plano plano, List<Taxa> taxas, DateTimeOffset dataSaida, DateTimeOffset dataRetornoPrevista,
+        DateTimeOffset? dataDevolucao, int quilometragemInicial, int? quilometragemFinal, int nivelCombustivelNaSaida, int? nivelCombustivelNaDevolucao,
+        bool seguroCliente, bool seguroTerceiro, double? valorSeguroPorDia, bool status) : this()
     {
         Condutor = condutor;
         Automovel = automovel;
@@ -36,8 +47,14 @@ public class Aluguel : EntidadeBase
         Taxas = taxas;
         DataSaida = dataSaida;
         DataRetornoPrevista = dataRetornoPrevista;
+        DataDevolucao = dataDevolucao;
         QuilometragemInicial = quilometragemInicial;
+        QuilometragemFinal = quilometragemFinal;
         NivelCombustivelNaSaida = nivelCombustivelNaSaida;
+        NivelCombustivelNaDevolucao = nivelCombustivelNaDevolucao;
+        SeguroCliente = seguroCliente;
+        SeguroTerceiro = seguroTerceiro;
+        ValorSeguroPorDia = valorSeguroPorDia;
         Status = status;
     }
 }
