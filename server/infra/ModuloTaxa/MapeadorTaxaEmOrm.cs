e9cc28819a473237b87a9f3716ce3f73a301d94d
iagodizlla <iagoschlemper@gmail.com>
2025-11-22 23:28:16 -0300

    Implementado Taxa em Infraestrutura

---
+++ server/infra/ModuloTaxa/MapeadorTaxaEmOrm.cs
@@ -0,0 +1,35 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloTaxa;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloTaxa;
+
+public class MapeadorTaxaEmOrm : IEntityTypeConfiguration<Taxa>
+{
+    public void Configure(EntityTypeBuilder<Taxa> modelBuilder)
+    {
+        modelBuilder.ToTable("TBTaxa");
+
+        modelBuilder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        modelBuilder.Property(b => b.Nome)
+            .HasColumnType("nvarchar(100)")
+            .IsRequired();
+
+        modelBuilder.Property(b => b.Preco)
+            .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+        modelBuilder.Property(b => b.Servico)
+            .HasColumnType("int")
+            .IsRequired();
+
+        modelBuilder
+            .HasOne(a => a.Empresa)
+            .WithMany()
+            .HasForeignKey(a => a.EmpresaId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ server/infra/ModuloTaxa/RepositorioTaxaEmOrm.cs
@@ -0,0 +1,8 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloTaxa;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloTaxa;
+
+public class RepositorioTaxaEmOrm(IContextoPersistencia context)
+    : RepositorioBase<Taxa>(context), IRepositorioTaxa;
