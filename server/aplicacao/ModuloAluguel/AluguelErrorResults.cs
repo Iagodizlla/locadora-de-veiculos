0fbce41021f149cabffa7737b5e62c6fa4082f7f
iagodizlla <iagoschlemper@gmail.com>
2025-11-23 18:11:02 -0300

    Implementado ErrorResults de Aluguel na Aplicacao

---
+++ server/aplicacao/ModuloAluguel/AluguelErrorResults.cs
@@ -0,0 +1,34 @@
+﻿using FluentResults;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloAluguel;
+
+public abstract class AluguelErrorResults
+{
+    public static Error CondutorNaoEncontradoError()
+    {
+        return new Error("Condutor requisitado não encontrado")
+            .CausedBy("Não foi possível obter o Condutor informado na requisição")
+            .WithMetadata("ErrorType", "BadRequest");
+    }
+
+    public static Error AutomovelNaoEncontradoError()
+    {
+        return new Error("Automóvel requisitado não encontrado")
+            .CausedBy("Não foi possível obter o Automóvel informado na requisição")
+            .WithMetadata("ErrorType", "BadRequest");
+    }
+
+    public static Error ClienteNaoEncontradoError()
+    {
+        return new Error("Cliente requisitado não encontrado")
+            .CausedBy("Não foi possível obter o Cliente informado na requisição")
+            .WithMetadata("ErrorType", "BadRequest");
+    }
+
+    public static Error PlanoNaoEncontradoError()
+    {
+        return new Error("Plano requisitado não encontrado")
+            .CausedBy("Não foi possível obter o Plano informado na requisição")
+            .WithMetadata("ErrorType", "BadRequest");
+    }
+}
