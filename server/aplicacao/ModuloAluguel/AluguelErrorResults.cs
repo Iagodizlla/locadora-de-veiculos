0525f8fda8a2439df9a66e782f2e1aa605e3c6dc
iagodizlla <iagoschlemper@gmail.com>
2025-11-25 05:13:07 -0300

    Implementado Validacoes de sequencia de entidades e excluir de Aluguel em Aplicacao

--- server/aplicacao/ModuloAluguel/AluguelErrorResults.cs
+++ server/aplicacao/ModuloAluguel/AluguelErrorResults.cs
@@ -31,4 +31,30 @@ public abstract class AluguelErrorResults
             .CausedBy("Não foi possível obter o Plano informado na requisição")
             .WithMetadata("ErrorType", "BadRequest");
     }
+    public static Error CondutorInvalidoParaClientePJError()
+    {
+        return new Error("Condutor inválido para Cliente do tipo PJ")
+            .CausedBy("Clientes PJ só podem selecionar condutores do tipo Não Cliente")
+            .WithMetadata("ErrorType", "BadRequest");
+    }
+
+    public static Error CondutorInvalidoParaClientePFError()
+    {
+        return new Error("Condutor inválido para Cliente do tipo PF")
+            .CausedBy("Clientes PF só podem selecionar condutores do tipo Cliente")
+            .WithMetadata("ErrorType", "BadRequest");
+    }
+
+    public static Error GruposAutomovelIncompativeisError()
+    {
+        return new Error("O Automóvel e o Plano pertencem a grupos diferentes")
+            .CausedBy("O grupo do Automóvel selecionado não corresponde ao grupo do Plano informado")
+            .WithMetadata("ErrorType", "BadRequest");
+    }
+    public static Error AluguelNaoPodeSerExcluidoError()
+    {
+        return new Error("Não é possível excluir este aluguel")
+            .CausedBy("O aluguel já está finalizado e não pode ser removido")
+            .WithMetadata("ErrorType", "BadRequest");
+    }
 }
