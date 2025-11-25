5e5be38854893d983422fcb0aa14a66d9b77726c
iagodizlla <iagoschlemper@gmail.com>
2025-11-25 07:29:02 -0300

    Implementado Config em Infraestrutura

---
+++ server/infra/ModuloConfig/MapeadorConfigEmOrm.cs
@@ -0,0 +1,38 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloConfig;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloConfig;
+public class MapeadorConfigEmOrm : IEntityTypeConfiguration<Config>
+{
+    public void Configure(EntityTypeBuilder<Config> modelBuilder)
+    {
+        modelBuilder.ToTable("TBConfig");
+
+        modelBuilder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        modelBuilder.Property(x => x.Gasolina)
+            .HasColumnType("decimal(10,2)")
+            .IsRequired();
+
+        modelBuilder.Property(x => x.Gas)
+            .HasColumnType("decimal(10,2)")
+            .IsRequired();
+
+        modelBuilder.Property(x => x.Diessel)
+            .HasColumnType("decimal(10,2)")
+            .IsRequired();
+
+        modelBuilder.Property(x => x.Alcool)
+            .HasColumnType("decimal(10,2)")
+            .IsRequired();
+
+        modelBuilder
+            .HasOne(a => a.Empresa)
+            .WithMany()
+            .HasForeignKey(a => a.EmpresaId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ server/infra/ModuloConfig/RepositorioConfigEmOrm.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraDeVeiculos.Dominio.Compartilhado;
+using LocadoraDeVeiculos.Dominio.ModuloConfig;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloConfig;
+
+public class RepositorioConfigEmOrm(IContextoPersistencia context)
+    : RepositorioBase<Config>(context), IRepositorioConfig
+{
+    public async Task<Config> SelecionarAsync()
+    {
+        return await registros.FirstOrDefaultAsync();
+    }
+
+    Task IRepositorioConfig.InserirAsync(Config configuracaoInicial)
+    {
+        return InserirAsync(configuracaoInicial);
+    }
+}
