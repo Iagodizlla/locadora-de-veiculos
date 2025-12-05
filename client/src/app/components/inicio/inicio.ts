a89a4f9d805c8ff29ae6597f3f59d17a5abe2f27
iagodizlla <iagoschlemper@gmail.com>
2025-12-05 13:34:19 -0300

    Implementado para card de Funcionario nao aparecer para usuario Funcionario

--- client/src/app/components/inicio/inicio.ts
+++ client/src/app/components/inicio/inicio.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
@@ -15,8 +17,11 @@ import { RouterLink } from '@angular/router';
     MatDividerModule,
     MatTooltipModule,
     RouterLink,
+    AsyncPipe,
   ],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss',
 })
-export class Inicio {}
+export class Inicio {
+  protected auth = inject(AuthService);
+}
