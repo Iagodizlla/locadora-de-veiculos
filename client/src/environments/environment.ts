910d7a1ed9032626cfbb72b1bce645bbb6e37b07
iagodizlla <iagoschlemper@gmail.com>
2025-12-10 15:39:25 -0300

    refatorado apiUrl

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gentle-tree-08ce2610f.3.azurestaticapps.net',
+  apiUrl: 'locadora-veiculo-ayhugeehdehhd6f5.brazilsouth-01.azurewebsites.net',
 };
